b0d531236bec7b14aea02c8e4dcc593b21a0df5e
yanjiel <yanjieliu829@gmail.com>
2023-02-18 21:19:44 -0600

    2nd commit - working w specified types

--- hw7.2.cpp
+++ hw7.2.cpp
@@ -1,52 +1,60 @@
 #include <iostream>
 #include <cmath>
 #include<future>
+#include<functional>
 
 using namespace std;
 
-//template<typename Func, typename ...Arg>
-//auto my_packaged_task(Func f, Arg ...args) {
-//	promise<int> p;
-//	future<int> f = p.get_future();
-//
-//	p.set_value(7);
-//	return f;
-//}
-
-template<typename Func>
-class my_packaged_task {
-public:
-    my_packaged_task() {}
-    my_packaged_task(Func f): fun(f) {
-        fv = p.get_future();
-    }
-
-    template<typename ...Arg>
-    int operator()(Arg ...args) {
-        
-
-        
-        return 1;
-    }
-
-private:
-    Func* fun;
-    promise<int> p;
-    future<int> fv;
-};
 
 double pow_of(int x, int y) { return pow(x, y); }
 
 void task_lambda()
 {
-    packaged_task<int(int, int)> task( [](int a, int b){return pow(a, b);} );
-    future<int> result = task.get_future();
+    packaged_task<double(int, int)> task( [](int a, int b){return pow(a, b);} );
+    future<double> result = task.get_future();
     task(2, 9);
     cout << "task_lambda:\t" << result.get() << '\n';
 }
 
+void func(std::promise<int>&& p) {
+    p.set_value(10);
+}
+
+
+template<typename Func>
+class my_packaged_task {
+public:
+    my_packaged_task(Func f) : fun(f) {}
+
+    template<typename ...Arg>
+    void operator()(Arg ...args) {
+        jthread jt([=] {do_work(args...); });
+    }
+
+    template<typename ...Arg>
+    void do_work(Arg ...args) {
+        double result = fun(args...);
+        p.set_value(result);
+    }
+   
+    future<double> get_future() {
+        return p.get_future();
+    }
+
+private:
+    Func* fun;
+    //future<double> fv;
+    promise<double> p;
+    
+};
+
+
+
 int main() {
-	task_lambda();
+	//task_lambda();
 	my_packaged_task<double(int, int)> task(pow_of);
-    cout << task(1, 2);
+    future<double> fd = task.get_future();
+    task(2, 3);
+    cout << fd.get();
+
 }
